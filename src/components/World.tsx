db4b713eb5404a14c6dd92bb7e1c3d028e68b555
Francesco Dammacco <francescodammacco@live.it>
2025-08-13 02:35:54 +0200

    Improve camera position

--- src/components/World.tsx
+++ src/components/World.tsx
@@ -1,11 +1,24 @@
-import { MeshDistortMaterial } from '@react-three/drei'
+import { CameraControls, MeshDistortMaterial } from '@react-three/drei'
+import { useThree } from '@react-three/fiber'
 import { RigidBody, useRapier } from '@react-three/rapier'
 import { button, useControls } from 'leva'
+import { useEffect } from 'react'
 import FishingRod from './FishingRod'
 
 export default function World() {
   const { step } = useRapier()
+  const { controls, viewport } = useThree()
+
   useControls('physics', { step: button(() => step(1 / 60)) })
+
+  // TODO improve
+  useEffect(() => {
+    if (!controls) return
+
+    const cameraControls = controls as CameraControls
+    cameraControls.truck(0, viewport.aspect < 1 ? -1 : -0.5, true)
+    cameraControls.dollyTo(10, true)
+  }, [controls])
 
   return (
     <>
@@ -14,7 +27,7 @@ export default function World() {
       <FishingRod position={240} color="limegreen" />
 
       <RigidBody type="fixed" colliders="hull" mass={100}>
-        <mesh receiveShadow scale={[2, 0.1, 2]}>
+        <mesh receiveShadow scale={[3, 0.1, 3]}>
           <cylinderGeometry />
           <MeshDistortMaterial color="dodgerblue" />
         </mesh>
