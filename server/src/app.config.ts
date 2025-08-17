e333541fa39c1547292e9846d6615c3ce6aecaf1
Francesco Dammacco <francescodammacco@live.it>
2025-08-17 02:25:17 +0200

    Add basic auth middleware to Colyseus monitor endpoint

--- server/src/app.config.ts
+++ server/src/app.config.ts
@@ -1,49 +1,30 @@
 import { monitor } from '@colyseus/monitor'
 import { playground } from '@colyseus/playground'
 import config from '@colyseus/tools'
-import { Request, Response } from 'express'
-
-/**
- * Import your Room files
- */
+import basicAuth from 'express-basic-auth'
 import { MyRoom } from './rooms/MyRoom'
 
 export default config({
   initializeGameServer: gameServer => {
-    /**
-     * Define your room handlers:
-     */
+    // Define your room handlers:
     gameServer.define('my_room', MyRoom)
   },
 
   initializeExpress: app => {
-    /**
-     * Bind your custom express routes here:
-     * Read more: https://expressjs.com/en/starter/basic-routing.html
-     */
-    app.get('/hello_world', (req: Request, res: Response) => {
-      res.send("It's time to kick ass and chew bubblegum!")
-    })
-
-    /**
-     * Use @colyseus/playground
-     * (It is not recommended to expose this route in a production environment)
-     */
     if (process.env.NODE_ENV !== 'production') {
       app.use('/', playground())
     }
 
-    /**
-     * Use @colyseus/monitor
-     * It is recommended to protect this route with a password
-     * Read more: https://docs.colyseus.io/tools/monitor/#restrict-access-to-the-panel-using-a-password
-     */
-    app.use('/monitor', monitor())
+    app.use(
+      '/monitor',
+      basicAuth({
+        users: { admin: process.env.MONITOR_PASSWORD },
+        challenge: true,
+      }),
+      monitor(),
+    )
   },
 
-  beforeListen: () => {
-    /**
-     * Before before gameServer.listen() is called.
-     */
-  },
+  // Before before gameServer.listen() is called.
+  beforeListen: () => {},
 })
