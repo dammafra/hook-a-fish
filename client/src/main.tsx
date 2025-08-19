13a2e93e9267956e2269a4853cb4a30114e44ca6
Francesco Dammacco <francescodammacco@live.it>
2025-08-19 16:29:39 +0200

    Fix theme color

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const themeColor = getComputedStyle(document.documentElement).getPropertyValue('--color-primary')
+document.querySelector('meta[name="theme-color"]')?.setAttribute('content', themeColor)
+
 createRoot(document.getElementById('root')!).render(<App />)
