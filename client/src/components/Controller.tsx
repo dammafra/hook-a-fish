8200906bf72f0c4bfc26fb06b3b7370252942bf2
Francesco Dammacco <francescodammacco@live.it>
2025-08-20 08:57:21 +0200

    Avoid `FishingRod` rotation reset on render

--- client/src/components/Controller.tsx
+++ client/src/components/Controller.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react'
+import { Euler } from 'three'
 import { useIsTouch } from '../hooks/use-is-touch'
 import useGame from '../stores/use-game'
 import { getPositionOnCirlce } from '../utils/position'
@@ -8,7 +9,9 @@ import PointerControls from './PointerControls'
 export default function Controller() {
   const isTouch = useIsTouch()
   const phase = useGame(state => state.phase)
+
   const initialPosition = useMemo(() => getPositionOnCirlce(2, 0, 2), [])
+  const initialRotation = useMemo(() => new Euler(0, 0, Math.PI * 0.35), [])
 
   return (
     phase !== 'ready' && (
@@ -18,8 +21,9 @@ export default function Controller() {
         lockPositionYAt={1.5}
         offset={isTouch ? [0, 0, -2] : 0}
         position={initialPosition}
+        rotation={initialRotation}
       >
-        <FishingRod rotation={[0, 0, Math.PI * 0.35]} />
+        <FishingRod />
       </PointerControls>
     )
   )
