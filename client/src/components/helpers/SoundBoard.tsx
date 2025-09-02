7a221507108a1f498bcebefa0243b250187d99e1
Francesco Dammacco <francescodammacco@live.it>
2025-09-03 01:01:08 +0200

    Cleanup

--- client/src/components/helpers/SoundBoard.tsx
+++ client/src/components/helpers/SoundBoard.tsx
@@ -66,6 +66,7 @@ export default function SoundBoard() {
   useEffect(() => {
     Howler.volume(muted ? 0 : 1)
   }, [muted])
+
   /**
    * This helps resume AudioContext when the tab is suspended (e.g., when switching apps or locking the phone) and later resumed,
    * especially on mobile where browsers often suspend audio contexts to save resources;
